d5562aa73b62262ba80a19d8ba13c401a3f61089
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-09 17:07:56 +0200

    fine

--- email/main.js
+++ email/main.js
@@ -1,24 +1,32 @@
-const email = prompt("Scrivi la tua mail");
-console.log(email);
+// const email = prompt("Scrivi la tua mail");
+// console.log(email);
 
-const listpeople = ["mattepelle.mp@gmail.com", "vvcbvcbvcvb", "vgfhgf", "hgfhgfhjghgbh", "jhvhjvhj",];
+function myFunction() {
+    let mailvera = document.getElementById("email").value;
+    console.log(mailvera);
 
-let check = false;
+    const listpeople = ["mattepelle.mp@gmail.com", "vvcbvcbvcvb", "vgfhgf", "hgfhgfhjghgbh", "jhvhjvhj",];
+    let check = false;
 
-for (let i=0; i< listpeople.length; i++) {
-    console.log(listpeople[i]);
 
-    if (email == listpeople[i]) {
-        check = true;
-       
+    for (let i=0; i< listpeople.length; i++) {
+        console.log(listpeople[i]);
+    
+        if (mailvera == listpeople[i]) {
+            check = true;
+           
+        }
+    } 
+    if (check == true) {
+        document.getElementById("mailgiuste").innerHTML = ` la mail: ${mailvera} è corretta`;
     }
-} 
-if (check == true) {
-    document.getElementById("mailgiuste").innerHTML = ` la mail: ${email} è corretta`;
+    else {
+        document.getElementById("mailgiuste").innerHTML = ` la mail: ${mailvera} è sbagliata`;
+    }
 }
-else {
-    document.getElementById("mailgiuste").innerHTML = ` la mail: ${email} è sbagliata`;
-}
+
+
+
 
 
 
